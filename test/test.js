da4b22a048148e3d712e93861df4aedaa4f1a915
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-01-01 01:51:11 +0300

    First commit

---
+++ test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+var path     = require('path');
+var generate = require('markdown-it-testgen');
+
+/*eslint-env mocha*/
+
+describe('markdown-it', function () {
+  var md = require('markdown-it')()
+              .use(require('../'));
+
+  generate(path.join(__dirname, 'fixtures/ins.txt'), md);
+});
